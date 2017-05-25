98d983b1584aea2b7cc9878070d286b4bf4c7a29
Rfulton <Reid.fulton1@gmail.com>
2017-05-24 21:30:45 -0400

    Receipt Program
    User enters and creates a receipt.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //class we made
+            Receipt new_receipt =  new Receipt();
+
+            //input statements
+            Console.WriteLine("Welcome new customer");
+            Console.WriteLine("Please enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your phone number in format 123456789");
+            string tempphone = Console.ReadLine();
+            char[] phone = tempphone.ToCharArray();
+            Console.WriteLine("Please enter your address all in one line");
+            string address = Console.ReadLine();
+            Console.WriteLine("Name of item Purchased?");
+            string item_name = Console.ReadLine();
+            Console.WriteLine("Number purchased?");
+            int num_items = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("I guess you get to enter the price of your item");
+            double unit_price = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+
+            //inputs the receipt
+            new_receipt.purchaseorder(name, address, phone, item_name, num_items, unit_price);
+            //prints the applicable information.
+            new_receipt.ToString();
+            Console.ReadKey(true);
+
+        }
+
+
+
+
+    }
+}
---
+++ Receipt.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receipt_Project
+{
+    public class Receipt
+    {
+        //variables asked to be stored in the receipt
+        Random rand2 = new Random();
+        string name;
+        int r_number = 0;
+        DateTime date_of_purchase;
+        int cust_num = 1000;
+        string cust_address;
+        char[] cust_phone = new char[9];
+        double unit_price;
+        int item_num;
+        string item_descrption;
+        int quantity_purchased;
+        double total_cost;
+        string item_name;
+
+        //default constructor, will create a blank receipt
+        public Receipt()
+        {
+
+
+
+
+
+        }
+        //to populate our receipt. 
+        public void purchaseorder(string name2,string cust_address2,char[] cust_phone2,string item_name2, int num_purchased, double unit_price2)
+            
+            
+            {
+            // sets item number to random number
+            item_num = rand2.Next(0, 1000);
+            //setting main storage variables
+            name = name2;
+            r_number++;
+            cust_num++;
+            date_of_purchase = DateTime.Now;
+            cust_address = cust_address2;
+            cust_phone = cust_phone2;
+            item_name = item_name2;
+           
+
+            quantity_purchased = num_purchased;
+            unit_price = unit_price2;
+
+            this.Calculate();
+
+
+            }
+
+        public override string ToString()
+        {
+            //string override to print
+            Console.Write("Customer Name: " + name + "\nCustomer Phone: " + cust_phone.ToString() + "\n Total Cost: " + total_cost);
+
+
+
+
+
+            return base.ToString();
+        }
+
+
+        public void Calculate()
+
+        {
+            
+            //calculates total price of receipt
+            total_cost = Convert.ToDouble(quantity_purchased) * unit_price;
+
+
+
+
+
+        }
+
+
+
+    }
+}
